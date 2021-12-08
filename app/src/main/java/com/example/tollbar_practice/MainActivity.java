96bc04809312fba111952966962328cb480a556c
gmdin <gmdin716@gmail.com>
2021-12-08 08:28:59 +0500

    MenuItems Added

--- app/src/main/java/com/example/tollbar_practice/MainActivity.java
+++ app/src/main/java/com/example/tollbar_practice/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.tollbar_practice;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
         Toolbar toolbar;
@@ -14,5 +20,30 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_items,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.action_tasks){
+            Toast.makeText(this, "Task Clicked", Toast.LENGTH_SHORT).show();
+        } else if(id==R.id.action_settings){
+            Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.about){
+            Toast.makeText(this, "Learn More Clicked", Toast.LENGTH_SHORT).show();
+        } else if (id==R.id.exit){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
